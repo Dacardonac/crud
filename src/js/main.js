34c225bccced97953026ffe3a20f3588caabffdf
Dacardonac <dacardonac201@gmail.com>
2024-05-15 10:09:50 -0500

    Octavo Commit (Se trabaja en le funciones)

--- src/js/main.js
+++ src/js/main.js
@@ -11,8 +11,31 @@ const form = document.getElementById("form");
 const name = document.getElementById("name");
 const lastName = document.getElementById("last-name");
 const email = document.getElementById("email");
+let idParaActualizar
 
 index(coders, tbody);
+
+form.addEventListener("submit", function (event) {
+
+    if (idParaActualizar === undefined) {
+        createCoderForm(nameCoder, lastNameCoder, emailCoder, coders)
+        showSmallAlertSUcces("Saved")
+    } else {
+        for (const coder of coders) {
+            if (coder.id == idParaActualizar) {
+                coder.name = nameCoder.value 
+                coder.lastName = lastNameCoder.value 
+                coder.email = emailCoder.value 
+            }
+        }
+
+        showSmallAlertSUcces("update")
+    }
+
+    index(coders, tbody)
+    event.preventDefault()
+    form.reset()
+})
 
 form.addEventListener('submit', function (event) {
 
@@ -27,15 +50,24 @@ form.addEventListener('submit', function (event) {
 });
 
 table.addEventListener('click', function (event) {
-    if (event.target.classList.contains("btn-danger")) {
-        const idParaEliminar = event.target.getAttribute("data-id");
 
-        coders.forEach((coder, index) => {
-            if (coder.id == idParaEliminar) {
-                coders.splice(index, 1);
-            };
-        });
-        index(coders, tbody);
-        showSmallAlertSuccess("Deleted");
-    };
-});
+    if (event.target.classList.contains('btn-danger')) {
+        let idDelete = Number(event.target.getAttribute('data-id'))
+        deleteItem(coders, idDelete)
+        showSmallAlertSUcces("Eliminado correctamente")
+        index(coders, tbody)
+
+    }
+    if (event.target.classList.contains("btn-warning")) {
+        idParaActualizar = event.target.getAttribute('data-id')
+        //vamos a buscar el usuario en la base de datos de manera declarativa
+        let userFound = coders.find(coder => coder.id == idParaActualizar)
+
+
+        //pintar los datos nuevamente en el formulario
+        nameCoder.value = userFound.name
+        lastNameCoder.value = userFound.lastName
+        emailCoder.value = userFound.email
+        console.log(userFound);
+    }
+})
